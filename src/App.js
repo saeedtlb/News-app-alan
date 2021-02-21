ff1130d4039aec03138711c17078b490ed6cb348
saeed-tlb <saeedt7879s@gmail.com>
2021-02-21 14:08:53 +0330

    wrap logo image to header tag, initialize alanBtn and call function on cammand

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web'
-require('dotenv').config()
 
 import actionByCommand from './command'
 import Cards from './components/Cards'
 
+require('dotenv').config()
+
 const App = () => {
+  const [articles, setArticles] = useState([])
+  const [activeArticle, setActiveArticle] = useState(0)
+
   useEffect(() => {
     alanBtn({
-      key: process.env.ALEN_KEY,
-      onCommand: actionByCommand
+      key: process.env.REACT_APP_ALEN_KEY,
+      onCommand: (command) => actionByCommand(command, setArticles, setActiveArticle)
     })
   }, [])
 
   return (
     <>
-      <div className="logo_container">
+      <header>
         <img src="/alan-logo.png" alt="alan-logo" />
-      </div>
-      <Cards />
+      </header>
+      <Cards articles={articles} activeArticle={activeArticle} />
     </>
   )
 }
